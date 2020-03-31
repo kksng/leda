8c43af95448899320d943d5900bd16111e9287c0
Andrew <Andrew5282011@gmail.com>
2020-03-31 10:04:02 +0300

    ProgressBar: add custom render for value render

--- leda/components/ProgressBar/types.ts
+++ leda/components/ProgressBar/types.ts
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import { PartialGlobalDefaultTheme } from '../../utils/useTheme';
 import { COMPONENTS_NAMESPACES } from '../../constants';
+import { CustomRender } from '../../commonTypes';
+import { SpanProps } from '../Span';
 
 export interface ProgressBarProps {
   /** Тема компонента */
   theme?: PartialGlobalDefaultTheme[typeof COMPONENTS_NAMESPACES.progressBar],
   /** Значение "завершенности" прогресс бара */
   value: number,
+  /** Реф */
   ref?: React.Ref<ProgressBarRefCurrent>,
+  /** Кастомный рендер для цифр со значением компонента, если цифры не нужны - передайте () => null */
+  valueRender?: CustomRender<ProgressBarProps, {}, SpanProps>,
   /** Классы переданные через _ */
   [x: string]: unknown,
 }
